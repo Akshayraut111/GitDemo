0c25d34e3046fede3a445a2a01c8391a2e2e0eea
akshay raut <akshayraut01031995@gmail.com>
2023-02-27 14:53:02 +0530

    archetect to asian development

--- src/main/java/Base_class/Base_class1.java
+++ src/main/java/Base_class/Base_class1.java
@@ -15,7 +15,20 @@ public class Base_class1 {
 		
 		driver.get("https://zerodha.com/products/kite");
 		driver.manage().window().maximize();
-		System.out.println("changes in git demo project");
+		System.out.println("changes in git demo projects");
+		return driver;
+		
+	}
+	
+	public static WebDriver getDriver1(String browser)
+	{
+	
+		System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\Browsers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		
+		driver.get("amzao.com");
+		driver.manage().window().maximize();
+		System.out.println("2nd tets");
 		return driver;
 		
 	}
